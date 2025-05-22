2807aa0790fa6f7783768ea3d11e1defdbe3507b
Edison <edison0929839399@gmail.com>
2025-05-23 00:01:52 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-
-export default function Dashboard() {
-  const [message] = useState('廠區管理系統')
-  
+export default function Home() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>{message}</h1>
-      <p>系統正在運行中...</p>
+    <div>
+      <h1>Hello World</h1>
     </div>
   )
 }
