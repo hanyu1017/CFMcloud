1ebd884f9d16d9d4fd4ee1b5639fda348314a7cd
Edison <edison0929839399@gmail.com>
2025-05-27 21:50:53 +0800

    new

--- next.config.cjs
+++ next.config.cjs
@@ -2,13 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  poweredByHeader: false,
   env: {
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_SERVER: process.env.DB_SERVER,
-    DB_NAME: process.env.DB_NAME,
-  },
-};
+    APP_URL: process.env.APP_URL,
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
